7083eedb70009c57b93d32cab3919ce1c3c891f5
Ryan Kim <rdhkim0@gmail.com>
2021-05-03 17:00:42 -0400

    added email portion

--- pythonscraper.py
+++ pythonscraper.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import openpyxl
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
-
+import smtplib
     
 # Setting chromedriver file path
 chromedriver = '/Users/ryankim/Downloads/chromedriver'
@@ -29,9 +28,23 @@ table = driver.find_element_by_xpath('//*[@id="pagebody"]/table[1]')
 # creating pandas dataframe object to export as excel file
 all_tables = pd.read_html(driver.page_source, attrs={'class': 'numeric full-width'})
 df = all_tables[0]
-
-# exporting the dataframe object as an excel sheet named output.xlsx and sheet name covid_19_stats
-df.to_excel("output.xlsx",sheet_name='covid_19_stats')
+percent_change, direction_change = df.iloc[1]['Percentage'].split('%')
+print(percent_change + direction_change)
 
 # closes google chrome window
 driver.quit()
+
+if float(percent_change) > 0.01 and str(direction_change) == ' increase':
+    #email code
+    FROM = "ryan.k0223@gmail.com"
+    TO = ["ryan.k0223@gmail.com"]
+    SUBJECT = "Covid-19 5% increase in cases since yesterday"
+    BODY = "Covid-19 cases in Ontario have increased by " + percent_change +" since yesterday/"
+    message = 'Subject: '+SUBJECT + '\n\n' +BODY
+    
+    # Send the mail
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("username@gmail.com", "password")
+    server.sendmail(FROM, TO, message)
+    server.quit()
